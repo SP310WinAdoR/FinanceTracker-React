461b9486c790de2d1061fd75f95c9f5d2884af08
DannyC44 <camilito101004@hotmail.com>
2022-07-19 18:23:30 -0500

    creacion de assets y home

---
+++ src/pages/home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import "../stylesSheets/homeStyle.css"
+
+export default function Home() {
+  return (
+    <div>
+        <div> alo </div> 
+        
+    </div>
+    
+
+  )
+}
