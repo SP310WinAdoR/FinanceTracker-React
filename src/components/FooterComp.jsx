eab4e339409b1747c8262d76ed87e7d5ef0c0a3f
DannyC44 <camilito101004@hotmail.com>
2022-07-21 04:20:04 -0500

    Home casi terminado y componente de Footer agregado

---
+++ src/components/FooterComp.jsx
@@ -0,0 +1,49 @@
+
+import React from 'react'
+import "../stylesSheets/Footer.css"
+export default function Footer() {
+
+    return (
+      <div>
+        <footer className='GridFooter'> 
+    
+            <div className="div1-Footer TitleFooter">Get In touch</div>
+            <div className="div2-Footer TitleFooter"> Compnay Info</div>
+            <div className="div3-Footer TitleFooter"> Features </div>
+            <div className="div4-Footer TitleFooter"> Resources </div>
+            <div className="div5-Footer"> The Quick for Jumps</div>
+            <div className="div6-Footer"> About Us </div>
+            <div className="div7-Footer"> Bussines Marketing</div>
+            <div className="div8-Footer"> GitHub</div>
+            <div className="div9-Footer SocialMediaIcons"> 
+            <ul class="wrapper">
+                <li class="icon facebook">
+                    <span class="tooltip">Facebook</span>
+                    <span><i class="fab fa-facebook-f"></i></span>
+                </li>
+                <li class="icon instagram">
+                    <span class="tooltip">Instagram</span>
+                    <span><i class="fab fa-instagram"></i></span>
+                </li>
+                <li class="icon github">
+                    <span class="tooltip">Github</span>
+                    <span><i class="fab fa-github"></i></span>
+                </li>
+               
+                </ul>
+            </div>
+            <div className="div10-Footer"> Carrier</div>
+            <div className="div11-Footer"> User Analytic </div>
+            <div className="div12-Footer"> Chart.JS</div>
+            <div className="div13-Footer">  </div>
+            <div className="div14-Footer">  We are Hiring</div>
+            <div className="div15-Footer"> More graphs</div>
+            <div className="div16-Footer"> ReactJs</div>
+
+           
+        </footer>
+      
+       
+      </div>
+    )
+  } 
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import "../stylesSheets/homeStyle.css";
 import NavBar from "../components/NavBar";
+import FooterComp from "../components/FooterComp"
 export default function Home() {
   return (
     <div>
@@ -18,12 +19,12 @@ export default function Home() {
                         <svg height="24" width="24" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="M0 0h24v24H0z" fill="none"></path><path d="M16.172 11l-5.364-5.364 1.414-1.414L20 12l-7.778 7.778-1.414-1.414L16.172 13H4v-2z" fill="currentColor"></path></svg>
                       </div>
                   </button>
-                  <a href="#" className="btn"> Más información </a>
+                  <button className='botonInfo'> Información </button>
                   </div>    
             </div>
             <div className='home-first-section_images-rigth'>
              <img className="home-blobMckp" src="../assets/blobsPhone.png" alt=""/>
-             <img className="home-phoneMckp" src="../assets/phoneMockup.png" alt=""/>
+             <img className="home-phoneMckp" src="../assets/PhoneFsection.png" alt=""/>
             </div> 
         </section>
 
@@ -35,6 +36,8 @@ export default function Home() {
           <h2 className='home-second-sectio_title'> ¡Explora nuestros servicios!</h2>
           <p className='home-second-sectio_description'>Problems trying to resolve the conflict between 
             the two major realms of Classical physics: Newtonian mechanics </p>
+
+          {/*TODO Crear un componente para los cards de la segunda seccioón   */}
           <div class="parent">
 
             <div className="div1 card-second-section"> 
@@ -70,9 +73,110 @@ export default function Home() {
           </div> 
 
         </section>
+      
+{/* Inicio de la tercera sección  */ }
+        <section className='home-third-section'> 
+
+            <div className='home-third-section_first-row'>
+
+              <h2 className='home-third-section_first-row_title titleFixMarign'> ¿Cómo funcionamos?  </h2>
+              <p className='home-third-section_first-row_p pFixMargin'> Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+              Lorem Ipsum has been the industry's
+              standard dummy text ever since the 1500s, when an unknown printer took a galley</p> 
+              <div className='home-third-section_first-row_card'> 
+
+                <img  src="../assets/graphCard.png" alt=""/> 
+                <h2 className='home-third-section_first-row_title'> Lorem Ipsum</h2>
+                <p className='home-third-section_first-row_p descriptionCard'> 
+                Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+                Lorem Ipsum has been the industry's</p>
+
+              </div>
+            </div> 
+          
+            <div className='home-third-section_second-row'> 
+              <div className='home-third-section_second-row_1card'>
+                <img  src="../assets/imageCard2row.png" alt=""/> 
+                <h2 className='home-third-section_first-row_title'>  Lorem Ipsum</h2>
+                <p className='home-third-section_first-row_p descriptionCard'> orem Ipsum is simply dummy text of the printing and typesetting industry. 
+                Lorem Ipsum has been the industry's</p>
+              </div>
+              <div className='home-third-section_second-row_2card' > 
+                <img src="../assets/tabDesign.png" alt=""/>
+                <article className='carTab_graph'> 
+                  <h6> Lorem Ipsum </h6> 
+                  <img src="./assets/Chart.png" alt="" width="120px"/> 
+                </article>
+                <article className='cardTab_circle'> 
+                    <div class="div1-section3"> -150 $</div>
+                    <div class="div2-section3"> +600$ </div>
+                    <div class="div3-section3"> Gastos </div>
+                    <div class="div4-section3">  Ganancias </div>
+                    <div class="div5-section3"> <img src="../assets/CircleImgCard.png" alt=""/> </div>
+                    <div class="div7-section3"> +1450$ </div>
+                    <div class="div9-section3"> Total semanal</div>
+                </article>  
+                <article className='cardTab_large'>
+                  <h5> Lorem Ipsum </h5> 
+                  <p> Lorem Ipsum fdfjds kasdjd sjfdsfjhfdsklm dkfjdsfkdjsf sdjfkls</p>
+                 </article>
+              </div>
+            </div> 
+        </section>
+{/* Cuarta Sección */ }
+
+        <section className="home-fourth-section"> 
         
+          <h1 className='home-fourth-section_title'> Precios </h1> 
+          <p className='home-fourth-section_descroption'>  Problems trying to resolve the conflict between 
+            the two major realms of Classical physics: Newtonian mechanics </p>
+          <div className='home-fourth-section_row'> 
+           
+              <CardPrice tipe="Free" description="Most calendars are designed 
+                                                    for teams. Slate is designed 
+                                                    for freelancers who want
+                                                      a simple way " price="0"/> 
+              
+              <CardPrice tipe="Standart" description="Most calendars are designed 
+                                                    for teams. Slate is designed 
+                                                    for freelancers who want
+                                                      a simple way " price="19"/> 
+    
+          </div>
+        </section> 
+
+        <FooterComp/> 
     </div>
     
-
   )
 }
+
+/* // TODO: Pasar estos componentes a la carpetas de "Components" */ 
+
+function CardPrice(props) {
+  return (
+    <div>
+      <section className='card_price'>
+        <div className='flex_card_price_col1'>
+          <h3 className='TituloCardPrice'> {props.tipe}</h3>
+          <p className='descriptionCardPrice'> {props.description}</p>
+        </div>
+        <div className='flex_card_price_col2'>        
+          <h2 className='PriceNumber'> {props.price}</h2>   
+          <div className='flex_card_price_col2_prices'> 
+            <span className='priceSign'>$</span>
+            <p className='priceSign'> Por mes </p> 
+            <button className='PriceButton'> Comprar
+              <div className="arrow-wrapper">
+                <div className="arrow"></div>
+              </div>
+            </button>    
+          </div> 
+            
+        </div>
+      </section>
+    </div>
+  )
+}
+
+
